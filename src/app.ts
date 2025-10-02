661b8d1dac5ea6e82c8103a5c52f4f6ec82a109b
Abu2013 <tesfayewalelign44@gmail.com>
2025-10-02 07:21:49 -0400

    chore: initialize backend folder structure and working dev server

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+export default app;
